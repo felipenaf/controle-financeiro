689902d85af0d1c3ef78f713f4d97aa5fa82db63
Felipe Ferreira <felipe.ferreira0192@gmail.com>
2019-03-09 16:38:31 -0300

    #2029 - ajuste na query

--- query.sql
+++ query.sql
@@ -2,6 +2,14 @@ DROP DATABASE IF EXISTS controle_financeiro;
 CREATE DATABASE controle_financeiro;
 USE controle_financeiro;
 
+CREATE TABLE usuario(
+	id_usuario int(11) NOT NULL AUTO_INCREMENT,
+	login varchar(255) NOT NULL,
+	senha varchar(255) NOT NULL,
+	nome varchar(255),
+	PRIMARY KEY (id_usuario)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE grupo (
 	id_grupo int(11) NOT NULL AUTO_INCREMENT,
 	nome varchar(100) NOT NULL,
@@ -12,12 +20,14 @@ CREATE TABLE grupo (
 
 CREATE TABLE produto (
 	id_produto int(11) NOT NULL AUTO_INCREMENT,
-	id_grupo int(11) NOT NULL,
+	id_grupo int(11),
+	id_usuario int(11),
 	descricao varchar(255) NOT NULL,
 	valor double(8,2) NOT NULL,
 	observacao text,
 	data_criacao timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	data_modificacao timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (id_produto),
-	FOREIGN KEY (id_grupo) REFERENCES grupo(id_grupo)
+	FOREIGN KEY (id_grupo) REFERENCES grupo (id_grupo),
+	FOREIGN KEY (id_usuario) REFERENCES usuario (id_usuario)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
